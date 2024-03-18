fe822f9088c02fd58f552dff54d00804753a879b
Henrique Antonelo <henrique.antonelo@gmail.com>
2024-03-18 18:02:01 -0300

    ... button animation

--- script.js
+++ script.js
@@ -224,3 +224,20 @@ document.getElementById("newGameBtn").addEventListener("click", (event) => {
 startGame();
 newGame();
 resetFocus();
+
+const dot1 = document.getElementById("dot-1");
+const dot2 = document.getElementById("dot-2");
+const dot3 = document.getElementById("dot-3");
+const mainBtn = document.querySelector(".dots-Btn");
+
+dot1.addEventListener("click", function () {
+  console.log("dot1");
+});
+
+dot2.addEventListener("click", function () {
+  console.log("dot2");
+});
+
+dot3.addEventListener("click", function () {
+  console.log("dot3");
+});
