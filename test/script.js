6114616bf5aaba891fbafef261b8e5197a113974
Henrique Antonelo <henrique.antonelo@gmail.com>
2024-04-02 00:00:03 -0300

    finished user container
    finished the user container and addded the infos button.
    added links to linkedin, github, pix, and comments.

--- test/script.js
+++ test/script.js
@@ -28,7 +28,42 @@
 // };
 
 ////////////////////
+// document.addEventListener("DOMContentLoaded", function () {
+//   const userContainer = document.querySelector(".user-container");
+//   const dropbtn = document.querySelector(".dropbtn");
+//   const dropdownContent = document.querySelector(".dropdown-content");
+//   const action = document.querySelector(".action");
+//   const username = document.querySelector(".user-name");
+//   const userProfilePic = document.querySelector(".fa-user-pen");
 
+//   dropbtn.addEventListener("click", function () {
+//     userContainer.classList.toggle("open");
+//     if (userContainer.classList.contains("open")) {
+//       dropdownContent.style.width = dropbtn.offsetWidth + "px";
+//     } else {
+//       dropdownContent.style.width = dropbtn.offsetWidth + "px";
+//     }
+//   });
+
+//   username.addEventListener("mouseover", function () {
+//     action.innerHTML = "View Profile";
+//   });
+//   userProfilePic.addEventListener("mouseover", function () {
+//     action.innerHTML = "Change photo";
+//   });
+//   document.addEventListener("mouseover", function (event) {
+//     if (event.target !== username && event.target !== userProfilePic) {
+//       action.innerHTML = "...";
+//     }
+//   });
+
+//   document.addEventListener("click", function (event) {
+//     if (!userContainer.contains(event.target) && event.target !== dropbtn) {
+//       userContainer.classList.remove("open");
+//     }
+//   });
+// });
+////////////////
 document.addEventListener("DOMContentLoaded", function () {
   const userContainer = document.querySelector(".user-container");
   const dropbtn = document.querySelector(".dropbtn");
@@ -37,9 +72,9 @@ document.addEventListener("DOMContentLoaded", function () {
   dropbtn.addEventListener("click", function () {
     userContainer.classList.toggle("open");
     if (userContainer.classList.contains("open")) {
-      dropdownContent.style.width = dropbtn.offsetWidth + "px";
+      dropdownContent.style.height = dropbtn.offsetheight + "px";
     } else {
-      dropdownContent.style.width = dropbtn.offsetWidth + "px";
+      dropdownContent.style.height = dropbtn.offsetheight + "px";
     }
   });
 });
